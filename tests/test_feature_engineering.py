da08b53726fd6d4ef2336150efe0db867cc1549b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-13 20:26:00 +0700

    :white_check_mark: add test model

---
+++ tests/test_feature_engineering.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from src.features.feature_engineering import create_features
+
+def test_create_features():
+    df = pd.DataFrame({'Close': [1, 2, 3]})
+    X, y = create_features(df)
+    assert isinstance(X, pd.DataFrame)
+    assert isinstance(y, pd.Series)
