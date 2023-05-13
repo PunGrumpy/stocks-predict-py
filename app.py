94b1b22de756df03abc355c23d7be8c3d0fc92f1
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-13 23:40:56 +0700

    :sparkles: change graph and ui

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import datetime
 import streamlit as st
+import plotly.graph_objects as go
 from src.data.data_preparation import fetch_data, preprocess_data
 from src.features.feature_engineering import create_features
 from src.models.train_model import train_model, save_model
@@ -18,13 +20,19 @@ stocks = {
         "currency": "THB",
     },
     "BBL.BK": {"name": "Bangkok Bank Public Company Limited", "currency": "THB"},
+    "KBANK.BK": {"name": "Kasikornbank Public Company Limited", "currency": "THB"},
 }
 
 # Ask user for stock symbol
 ticker = st.selectbox("Select a stock symbol:", list(stocks.keys()))
 
+# Let the user select the number of days
+days = st.slider(
+    "Number of past days to consider:", min_value=15, max_value=120, value=30
+)
+
 # Calculate the start date as 30 days ago
-start_date = date.today() - timedelta(days=30)
+start_date = date.today() - timedelta(days=days)
 
 # Fetch and preprocess data
 df = fetch_data(ticker, start_date)
@@ -40,17 +48,6 @@ else:
     # Train the model
     model, mae, mse, r2, cv_score = train_model(X, y)
 
-    # Display performance metrics
-    st.write(
-        f"""
-            Performance Metrics:
-            * Mean Absolute Error (MAE): {mae:.2f}
-            * Mean Squared Error (MSE): {mse:.2f}
-            * R^2 Score: {r2:.2f}
-            * Cross-Validation Score: {cv_score:.2f}
-        """
-    )
-
     # Save the model
     save_model(model, "models/model.pkl")
 
@@ -62,7 +59,60 @@ else:
 
     # Streamlit interface
     st.title(f'Stock Price Prediction for {stocks[ticker]["name"]}')
-    st.line_chart(df["Close"])
-    st.write(
-        f'The predicted closing price for the next day is: {round(prediction[0], 2)} {stocks[ticker]["currency"]}'
+
+    # Plot the actual values vs predictions using Plotly
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=df.index,
+            y=df["Close"],
+            mode="lines",
+            name="Actual values",
+            line=dict(color="blue", width=2),
+        )
     )
+    fig.add_trace(
+        go.Scatter(
+            x=df.index,
+            y=loaded_model.predict(X),
+            mode="lines",
+            name="Predicted values",
+            line=dict(color="red", width=1),
+        )
+    )
+
+    fig.update_layout(
+        title=f"Actual vs Predicted Stock Prices for {stocks[ticker]['name']}",
+        xaxis_title="Date",
+        yaxis_title=f"Price (in {stocks[ticker]['currency']})",
+        legend_title="Legend",
+        font=dict(family="Courier New, monospace", size=18, color="RebeccaPurple"),
+    )
+
+    st.plotly_chart(fig)
+
+    st.write(
+        f'The predicted closing price for the next day is: **{round(prediction[0], 2)} {stocks[ticker]["currency"]}**'
+    )
+
+    # Advise the user on whether to buy or sell
+    current_price = df["Close"].iloc[-1]
+    if prediction[0] > current_price:
+        st.write(
+            f"The predicted closing price is higher than the current closing price. This could be a good opportunity to buy {ticker}."
+        )
+    elif prediction[0] < current_price:
+        st.write(
+            f"The predicted closing price is lower than the current closing price. You might want to consider selling {ticker}."
+        )
+    else:
+        st.write(
+            f"The predicted closing price is equal to the current closing price. You might want to hold onto your {ticker} stocks or monitor the market further."
+        )
+
+    # Display performance metrics
+    st.subheader("Model Performance Metrics:")
+    st.markdown(f"* Mean Absolute Error (MAE): {mae:.2f}")
+    st.markdown(f"* Mean Squared Error (MSE): {mse:.2f}")
+    st.markdown(f"* R^2 Score: {r2:.2f}")
+    st.markdown(f"* Cross-Validation Score: {cv_score:.2f}")
