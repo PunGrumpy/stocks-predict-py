dddef30a6e11e1500fd7af4f763455704b28d733
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-13 19:36:48 +0700

    :bug: wrong ignore files

---
+++ src/models/predict_model.py
@@ -0,0 +1,11 @@
+import pickle
+import numpy as np
+
+def load_model(path):
+    with open(path, 'rb') as f:
+        loaded_model = pickle.load(f)
+    return loaded_model
+
+def make_prediction(model, X):
+    prediction = model.predict(X)
+    return prediction
